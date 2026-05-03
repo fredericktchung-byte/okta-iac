526f749b152662361e2da7726d422a58f09f36b0
Fred Chung <frederick.t.chung@gmail.com>
2026-05-03 13:17:00 -0700

    Refactor: Remove legacy generated files to begin clean slate rebuild

--- policies.tf
+++
@@ -1,6 +0,0 @@
-resource "okta_policy_signon" "default" {
-  name            = "Default Policy"
-  status          = "ACTIVE"
-  description     = "The default policy applies in all situations if no other policy applies."
-  groups_included = [data.okta_everyone_group.everyone.id]
-}
