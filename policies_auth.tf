f06e25ca0f1694201acfbec3798a0ec2470b3f94
Fred Chung <frederick.t.chung@gmail.com>
2026-05-06 17:32:16 -0700

    feat(phase-2): safely import and manage default mfa policy

--- policies_auth.tf
+++ policies_auth.tf
@@ -14,3 +14,14 @@ resource "okta_policy_mfa" "service_accounts" {
   # Hardcoded Okta ID for 'Okta - Role - Service Accounts' to prevent detachment
   groups_included = ["00g11aeo1g4KtFPix698"]
 }
+
+# Authenticator Enrollment Policy: Default
+resource "okta_policy_mfa" "default" {
+  name        = "Default Policy"
+  status      = "ACTIVE"
+  description = "The default policy applies in all situations if no other policy applies."
+  
+  is_oie      = true
+
+  groups_included = ["00g1100e6nrqyrHst698"]
+}
