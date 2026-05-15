a48c4dd309f81546bfebe50c6c2dc187f00442db
Fred Chung <frederick.t.chung@gmail.com>
2026-05-15 16:34:49 -0700

    feat: added Workato application assignment group

--- apps.tf
+++ apps.tf
@@ -27,24 +27,9 @@ resource "okta_app_saml" "oracle_cloud" {
 
 # Workato SAML Integration
 resource "okta_app_saml" "workato" {
-  label = "Workato"
+  label             = "Workato"
+  preconfigured_app = "workato"
 
-  # The SSO Destination Endpoints
-  sso_url     = "https://www.workato.com/sso/saml/consume"
-  recipient   = "https://www.workato.com/sso/saml/consume"
-  destination = "https://www.workato.com/sso/saml/consume"
-
-  # The strict Entity ID / Provider ID expected by Workato
-  audience = "https://www.workato.com/saml/metadata"
-
-  # Workato requires the NameID to be the email address, mapped precisely
-  subject_name_id_template = "$${user.email}"
-  subject_name_id_format   = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
-
-  # SAML Configuration
-  response_signed         = true
-  signature_algorithm     = "RSA_SHA256"
-  digest_algorithm        = "SHA256"
-  honor_force_authn       = true
-  authn_context_class_ref = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
+  # Because it's an OIN app, Okta automatically populates the SSO URLs, 
+  # Audience Restrictions, and standard Attribute Statements for you!
 }
