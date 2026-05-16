0649e87ef2f4e20e388711aa87aa65fc988d9d91
Fred Chung <frederick.t.chung@gmail.com>
2026-05-16 05:52:31 -0700

    feat: Add Workato M2M API Service for Workato Connection

--- apps.tf
+++ apps.tf
@@ -33,3 +33,39 @@ resource "okta_app_saml" "workato" {
   # Because it's an OIN app, Okta automatically populates the SSO URLs, 
   # Audience Restrictions, and standard Attribute Statements for you!
 }
+
+# =============================================================================
+# 🤖 MACHINE-TO-MACHINE (Phase 3 -> Phase 4 Bridge)
+# =============================================================================
+
+# Workato API Access Application (OIDC / Client Credentials)
+resource "okta_app_oauth" "workato_m2m" {
+  label       = "Workato API Integration"
+  type        = "service"
+  grant_types = ["client_credentials"]
+
+  # Enforcing high-security cryptographic authentication
+  token_endpoint_auth_method = "private_key_jwt"
+
+  # Injecting the Public Key for Okta to verify Workato's signature
+  jwks {
+    kty = "RSA"
+    kid = "5IzcQecwWliLbmPGx9bVD7UTYgIx/ulP9jYW4BQRGe0=" # A unique identifier we make up
+    e   = "AQAB"
+    n   = "0f1NLP5rqh9HhEcopXri0OcUaTKy65U6y5JaKfRZ4vniEPK8WlPDJOCxCZZ-VSYT439VsFEM-DUyFiG7CdX5G-JijFF2AeIZ5k1CDo8rn_e-cBN-0-PjKFvV-xGA4If1QckyCKSMLp2hcAUUqzrVa1W5hf-8dHV3YAp4DOU4eyb5rUcncBX0MH3w0ashOjG7vdYz0xAsfGVwjjgyg5QS9rAdwDR5gyj6OGr7Jni7WMy6a-er34ZMfgdSDc65SVr7Sxr_MRlRdZW-ZNINfvCEWhVYKhVWw0Gk9sn4kL5mgvnztiTVPMrBGDETJUF6GUol3afZiTfKDunskcgQSExKyQ"
+  }
+}
+
+# Authorize the M2M App to manage Okta Directory components
+resource "okta_app_oauth_api_scope" "workato_scopes" {
+  app_id = okta_app_oauth.workato_m2m.id
+  issuer = "https://integrator-1501452.okta.com"
+  scopes = [
+    "okta.users.manage",
+    "okta.groups.manage",
+    "okta.schemas.read",
+    "okta.logs.read",
+    "okta.eventHooks.manage",
+    "okta.apps.read"
+  ]
+}
