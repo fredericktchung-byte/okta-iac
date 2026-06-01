64667b945fdeecd17da2849711d6d35ec4f7ddaa
Fred Chung <frederick.t.chung@gmail.com>
2026-06-01 06:58:42 -0700

    OKTA-44 Add bookmark for Notion

--- apps.tf
+++ apps.tf
@@ -363,3 +363,10 @@ resource "okta_app_group_assignment" "grafana_viewers" {
   app_id   = okta_app_oauth.grafana.id
   group_id = okta_group.app_grafana_viewers.id
 }
+
+# Notion bookmark integration
+resource "okta_app_bookmark" "notion" {
+  label                 = "Notion"
+  url                   = "https://app.notion.com/p/Welcome-to-Notion-37201dd05b8280d8b475ea3294aeda36" # This is the URL users will be directed to when they click the bookmark in Okta. It can be the generic login page since our authentication policy will allow access with an active session, preventing double prompts.
+  authentication_policy = okta_app_signon_policy.bookmark_apps.id # Binds the bookmark app to the relaxed policy that allows access with an active session, preventing double prompts for users who are already authenticated to Okta.
+}
