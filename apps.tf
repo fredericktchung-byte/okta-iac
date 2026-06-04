8d23e472ca5cf711fa8eaafce66538ec0331cf74
Fred Chung <frederick.t.chung@gmail.com>
2026-06-04 14:15:46 -0700

    bugfix/remove-codespace-file-and-fix-zapier-bookmark

--- apps.tf
+++ apps.tf
@@ -393,7 +393,7 @@ resource "okta_app_group_assignment" "figma_users" {
 # Zapier bookmark integration
 resource "okta_app_bookmark" "zapier" {
   label                 = "Zapier"
-  url                   = "https://zapier.com/app/dashboard"     # This is the URL users will be directed to when they click the bookmark in Okta. It can be the generic login page since our authentication policy will allow access with an active session, preventing double prompts.
+  url                   = "https://zapier.com/app/dashboard"      # This is the URL users will be directed to when they click the bookmark in Okta. It can be the generic login page since our authentication policy will allow access with an active session, preventing double prompts.
   authentication_policy = okta_app_signon_policy.bookmark_apps.id # Binds the bookmark app to the relaxed policy that allows access with an active session, preventing double prompts for users who are already authenticated to Okta.
 }
 
@@ -401,7 +401,7 @@ resource "okta_app_bookmark" "zapier" {
 resource "okta_app_group_assignment" "zapier_users" {
   app_id   = okta_app_bookmark.zapier.id
   group_id = okta_group.app_zapier_users.id
-} 
+}
 
 
 # =============================================================================
